97ec79a1d6c04357e702976340d88063f9c750b3
Dxy1307 <ngducduy137@gmail.com>
2024-01-30 16:51:54 +0700

    link to MySQL

--- Project/src/main/java/com/webgiasu/config/JPAConfig.java
+++ Project/src/main/java/com/webgiasu/config/JPAConfig.java
@@ -49,17 +49,17 @@ public class JPAConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/cnpm");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/project1");
         dataSource.setUsername("root");
-        dataSource.setPassword("Nguyenducduy1307@");
+        dataSource.setPassword("root");
         return dataSource;
     }
 
     Properties additionalProperties() {
         Properties properties = new Properties();
 //        properties.setProperty("hibernate.hbm2ddl.auto", "create");
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-//        properties.setProperty("hibernate.hbm2ddl.auto", "none");
+        // properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.hbm2ddl.auto", "none");
         properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
         return properties;
 }
